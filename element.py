1c9ea4a13888b12f2660cad1d119adcd219be238
lolle97 <lorenzo.wormer@gmail.com>
2014-05-21 09:02:33 +0200

    Update element.py

--- element.py
+++ element.py
@@ -1,2 +1,27 @@
-class element(object):
+
+class Element(object):
   
+  def __new__(self, name, parent):
+    if isinstance( name, str ) and isinstance( parent, Folder ):
+      self.__name = name
+      self.__changeDate = datetime.datetime.now()
+      self.__parent = parent
+    else raise Exception()
+
+  def getName(self):
+    return self.__name
+    
+  def getSize(self):
+    return 0
+  
+  def getChangeDate(self):
+    return self.__changeDate
+  
+  def getInfo(self):
+    return
+  
+  def setName(self, name):
+    if isinstance( name, str ):
+      self.__name = name
+      return true
+    else raise Exception()
