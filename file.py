39d3af8d339d0003c815c2b36a335a36d1b3f8c6
lolle97 <lorenzo.wormer@gmail.com>
2014-05-21 09:14:24 +0200

    Update file.py

--- file.py
+++ file.py
@@ -1,3 +1,14 @@
 
 class File(Element):
   
+  def __init__(self, name, parent, size):
+    super.__init__(name, parent)
+    if isinstance(size, int):
+      self.__size = size
+    else raise Exception()
+  
+  def getSize():
+    return size
+  
+  def getInfo():
+    return ""
