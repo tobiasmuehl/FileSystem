6b395f20208d1b995d4497f2bb246961f7ad2d47
tobiasmuehl <tobiasmuehl@me.com>
2014-05-21 08:58:58 +0200

    getSize()

--- folder.py
+++ folder.py
@@ -1,3 +1,13 @@
 class Folder(Element):
+  def __init__(self, name, size):
+    super().__init__(name, size)
+    self.__contains = [];
+
   def getSize(self):
+    self.__size = 0
+    for element in self.__contains:
+      self.__size += element.getSize()
+    return self.__size
+    
+  
     
