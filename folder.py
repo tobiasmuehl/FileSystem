509d4aadc54def2016e26ca841d08a02050055ff
tobiasmuehl <tobiasmuehl@me.com>
2014-05-22 12:02:13 +0200

    Update folder.py

--- folder.py
+++ folder.py
@@ -1,13 +1,14 @@
 class Folder(Element):
-  root = None;
+  __root = None;
+  __changeDate = None;
   
   def __init__(self, name, size, parent):
     super().__init__(name, size, parent)
     self.__contains = [];
     
-    if parent == None and root == None:
-      root = self
-    elif parent == None and isinstance(root, Folder):
+    if parent == None and __root == None:
+      __root = self
+    elif parent == None and isinstance(__root, Folder):
       raise Exception("Root Folder already exists")
 
   def getSize(self):
@@ -20,10 +21,24 @@ class Folder(Element):
     self.__contains.push(file)
 
   def drawTree():
-    drawTree(root)
+    drawTree(__root)
 
   def drawTree(self):
     for x in obj.__contains:
       if isinstance(x, Folder):
         print(x.getName())
         drawTree(x)
+  
+  def getChangeDate(self):
+    for x in obj.__contains:
+      if isinstance(x, Folder):
+        x.getChangeDate()
+        
+      if isinstance(x, File):
+        new = x.getChangeDate();
+      
+      if new > changeDate:
+        changeDate = new
+    
+    return new
+    
