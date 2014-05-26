5d0ca685c95aefc1beb9e4a9cbcc7b331c770112
Tobias Mühl <t.muehl@crowdofcreatives.de>
2014-05-26 20:16:06 +0200

    intendation

--- FSfolder.py
+++ FSfolder.py
@@ -6,17 +6,17 @@ class Folder(Element):
     root = None
     
     def __init__(self, name, parent):
-        Element.__init__(self, name, parent)
-        if parent == None:
-            if Folder.root == None:
-                Folder.root = self
-                parent = self
-            else: raise Exception()
-        else:
-            if Folder.root == None:
-                raise Exception()
-            else: self.__parent = parent
-        self.__content = [];
+      Element.__init__(self, name, parent)
+      if parent == None:
+          if Folder.root == None:
+            Folder.root = self
+            parent = self
+          else: raise Exception()
+      else:
+          if Folder.root == None:
+            raise Exception()
+          else: self.__parent = parent
+      self.__content = [];
     
     def drawTree():
         Folder.root.drawFolders(0)
@@ -24,18 +24,18 @@ class Folder(Element):
     def drawFolders(self, level):
         space = ""
         for i in range(0, level):
-            space += "+"
+          space += "+"
       
         print(space, self.getName())
     
         for x in self.__content:
-            if isinstance(x, Folder):
-                x.drawFolders(level+1)
+          if isinstance(x, Folder):
+            x.drawFolders(level+1)
 
     def getSize(self):
         self.__size = 0
         for element in self.__contains:
-            self.__size += element.getSize()
+          self.__size += element.getSize()
         return self.__size
 
     def addElement(self, e):
@@ -44,9 +44,9 @@ class Folder(Element):
     def getChangeDate(self):
         last = self.__changeDate
         for x in self.__content:
-            changeDate = x.getChangeDate();
-            if changeDate > last:
-                last = changeDate
+          changeDate = x.getChangeDate();
+          if changeDate > last:
+            last = changeDate
         return last
 
     def setRoot(root):
